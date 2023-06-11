fbf0679652733075ffcae05b0a7dae7b3b37ebc5
Nour Awawdeh <117414786+NOUR-AWAWDEH@users.noreply.github.com>
2023-06-11 12:04:46 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,35 +3,28 @@
 // beecrowd | 1037 : Interval
 int main()
 {
-
   double number;
   scanf("%lf", &number);
-
   if (number < 0 || number > 100)
   {
-
     printf("Fora de intervalo\n");
-
   }
   else if (number <= 25)
   {
-
     printf("Intervalo [0,25]\n");
   }
   else if (number <= 50)
   {
-
     printf("Intervalo (25,50]\n");
   }
   else if (number <= 75)
   {
-
     printf("Intervalo (50,75]\n");
   }
   else
   {
-
     printf("Intervalo (75,100]\n");
   }
+  
   return 0;
 }
